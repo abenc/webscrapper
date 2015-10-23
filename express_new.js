6d096c13278e29fb2a153dfddf52d9763ed0f816
Anir <anir.benchabane@live.fr>
2015-10-24 00:32:06 +0200

    first commit

---
+++ express_new.js
@@ -0,0 +1,28 @@
+var express=require('express');
+var session=require('cookie-session');
+var bodyParser = require('body-parser');
+var leboncoin = require('leboncoin_new');
+var lacentrale = require('lacentrale_new');
+var urlencodedParser = bodyParser.urlencoded({extended:true});
+
+
+var app = express();
+
+app.use(session({
+  secret:'scraper'
+}),express.static(__dirname + '/public'))
+
+
+//routes
+
+.get('/',leboncoin.initScrap)
+
+.post('/scrapleboncoin',urlencodedParser,leboncoin.postAndScrap)
+
+.get('/scraplacentrale',lacentrale.scrapLacentrale)
+
+.get('/scraplacentrale/:url',lacentrale.scrapLacentralePack)
+
+
+//end routes
+.listen(8080);
